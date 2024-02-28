08f9d5c621669cde5caa4ed2196c0b2ab60deeef
DieryVale <dieryvale.01@gmail.com>
2024-02-27 22:20:19 -0500

    metodos

--- simulador/SimuladorBancario.py
+++ simulador/SimuladorBancario.py
@@ -8,3 +8,29 @@ class simuladorBancario:
     cedula = 0
     nombre=''
     ingresAct = 0
+    valor = 0
+
+    '''-----------------
+    #Metodos
+    ------------------'''
+
+    def consignarValor(self, cValor):
+        #aqui va el codigo
+        self.valor = cValor
+        return "El valor consignado es: " + self.valor
+
+
+    def consultarSaldo(self):
+        #aqui va el codigo
+        return "Su saldo es: " + self.saldo
+
+
+
+    def retirarValor(self):
+        #aqui va el codigo
+        return "El valor retirado es: " + self.retirar
+
+
+    def consultarInteresMensual(self):
+        #aqui va el codigo
+        return "Los interes mensuales son: " + self.conInteres
