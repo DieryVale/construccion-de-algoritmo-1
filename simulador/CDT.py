f8a721fcae70e432d6bb5c1437d4b450361f5718
Diery Valencia <112973080+DieryVale@users.noreply.github.com>
2024-02-29 16:45:10 -0500

    Update CDT.py
    Metodos

--- simulador/CDT.py
+++ simulador/CDT.py
@@ -7,3 +7,13 @@ class CDT:
     ValorInvertido = 0
     interes = 0
     mesApertura = 0 
+
+     '''-----------------
+     # Metodos
+    --------------------'''
+     
+
+    def  calcular_interes ( intereses ):
+        cantidad = 0
+        intereses  =  cantidad  *  0.0
+        return  intereses
