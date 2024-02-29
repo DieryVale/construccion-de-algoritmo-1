5d4f734f1e97c818a9a8966ac8561f3ab4fc109d
Diery Valencia <112973080+DieryVale@users.noreply.github.com>
2024-02-29 16:43:59 -0500

    Update Empledo.py
    Metodos

--- empleado/Empledo.py
+++ empleado/Empledo.py
@@ -31,6 +31,38 @@ class Empleado:
         self.salario = nSalario
         return "Su nuevo salario es: " + self.salario
 
+     def DuplicarSalario(self):
+        #forma 1
+        nuevoSalario = self.salario * 2
+        self.salario = nuevoSalario
+
+        # forma 2
+        # self.salario *= 2
+
+
+    def CalcularSalarioAnual(self): #calcular el impuesto anual del 19.5 iba 
+        # forma 1
+        salariAnual = self.salrio * 12        
+        return salariAnual 
+
+        #forma 2
+        # return "Su salario anual es: " + self.salario*12 cont k + c, contr k + u
+    
+    def ConsultarDiaCumpleaños(self):
+        return self.fechaNacimiento.ConsultarDia() 
+    
+    def CalcularImpuestos(self):
+        # forma 1
+        total = self.CalcularSalarioAnual()
+        total = total * (19.5/100)
+        return total
+
+        #forma 2        
+        # return self.CalcularSalarioAnual() * 0.195
+    
+    # hacer lo siguientes metodos agregar las asociaciones en el caso dos, cuenta bancaria  cosignar cuenta corriente, calcular saldo total 
+    # cuenta de ahorro. Cuenta corriente, pasar de ahorros a corriente 
+
 
     
 
