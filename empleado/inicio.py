da30aecce6a69e702d404b7d9dcc2c7a3286d83a
DieryVale <dieryvale.01@gmail.com>
2024-03-03 00:54:16 -0500

    inicio

---
+++ empleado/inicio.py
@@ -0,0 +1,7 @@
+from Empledo import Empleado
+
+empleado1 = Empleado('Juan', 'Perez', 1000000, 1) # clonar liena alt shift + flecha abajo
+empleado1 = Empleado('Pedro', 'Martinez', 6000000, 1)
+empleado1 = Empleado('Maria', 'Benitez', 8000000, 2)
+
+print(empleado1.ConsultarSalario())
